f0a0171e448812c0675bf863996207ad5f9a882b
bvermeire <bvermeire@gmail.com>
2018-05-09 23:39:38 +0200

    changing editscreen (progress)

--- client/src/components/EditItem.vue
+++ client/src/components/EditItem.vue
@@ -1,17 +1,27 @@
 <template>
-<v-list>
+<div>
+<v-form ref="form" v-model="valid" lazy-validation v-if="itemnameview" color="primary" dark>
+      <v-text-field
+        v-model="site.url"
+        :counter="30"
+        label="Site Name"
+        required
+        append-icon="save" :append-icon-cb="saveSite"
+      ></v-text-field>
+</v-form>  
+<v-list v-if="!itemnameview">
   <v-list-tile v-for="site in sitenames" :key="site._id">
-    <v-list-tile-action>
-    </v-list-tile-action>
     <v-list-tile-content>
-      <v-list-tile-title v-text="site.url">
-        test
-      </v-list-tile-title>
+      <v-list-tile-title v-text="site.url"></v-list-tile-title>
     </v-list-tile-content>
-    <v-list-tile-avatar>
-    </v-list-tile-avatar>
+    <v-list-tile-action v-if="!itemnameview">
+      <v-btn icon ripple @click="editname(site)">
+        <v-icon color="grey lighten-1" >info</v-icon>
+      </v-btn>
+    </v-list-tile-action>
   </v-list-tile>
 </v-list>      
+</div>
 </template>
 <script>
 /* eslint-disable */
@@ -25,8 +35,10 @@ export default {
       sitenames: [],
       wantedprice: null,
       itemId: '/',
-      e6:1
-
+      e6:1,
+      itemnameview: false,
+      valid: false,
+      site: ''
     };
   },
   mounted(){
@@ -47,6 +59,13 @@ export default {
       catch (error) {
         console.log(error)
       }
+    },
+    editname(site){
+      this.itemnameview=!this.itemnameview
+      this.site = site
+    },
+    saveSite() {
+
     }
   }
 }
